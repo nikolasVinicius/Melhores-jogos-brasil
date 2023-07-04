906b0c4aa7f602b25ceb52f621f0049ab325719d
nikolasVinicius <regamemine2.0@gmail.com>
2023-07-04 19:00:44 -0300

    v3

---
+++ public/frontend/login/js/script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn-register')
+console.log('tudo ok 3')
+button.addEventListener('click',()=>{
+   window.location = "../register/index.html"
+})
---
+++ public/frontend/register/js/script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn-login')
+console.log('tudo ok 3')
+button.addEventListener('click',()=>{
+   window.location = "../login/index.html"
+})
